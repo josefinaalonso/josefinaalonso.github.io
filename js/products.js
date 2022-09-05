dd4221038009df30947617b3bc2d973641430883
Josefina Alonso <joseaillen@hotmail.com>
2022-09-04 22:41:11 -0300

    Entrega dos

--- js/products.js
+++ js/products.js
@@ -1,5 +1,6 @@
-const url = 'https://japceibal.github.io/emercado-api/cats_products/101.json';
+/* const url = `https://japceibal.github.io/emercado-api/cats_products/${localStorage.getItem("catID")}.json`;
 let productsArray = [];
+
 fetch(url)
 .then(function(respuesta) {
     return respuesta.json()
@@ -27,6 +28,147 @@ fetch(url)
         `; 
     } 
     divListaProducts.innerHTML += htmlContentToAppend;  
-    console.log(productsArray)
     });
-    
+     */
+
+const ORDER_ASC_BY_PRICE = "AZ";
+const ORDER_DESC_BY_PRICE = "ZA";
+const ORDER_BY_PROD_REL = "Cant.";
+let currentCategoriesArray = [];
+let currentSortCriteria = undefined;
+let minCount = undefined;
+let maxCount = undefined;
+
+function sortCategories(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_PRICE)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.cost < b.cost ){ return -1; }
+            if ( a.cost > b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_PRICE){
+        result = array.sort(function(a, b) {
+            if ( a.cost > b.cost ){ return -1; }
+            if ( a.cost < b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_BY_PROD_REL){
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
+
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+}
+
+
+function showCategoriesList(){
+
+    let htmlContentToAppend = "";
+    for (let productsArray of currentCategoriesArray)
+        if (((minCount == undefined) || (minCount != undefined && parseInt(productsArray.cost) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(productsArray.cost) <= maxCount))){
+
+            htmlContentToAppend += `
+            <div onclick="setCatID(${productsArray.id})" class="list-group-item list-group-item-action cursor-active">
+            <div class="row">
+                <div class="col-3">
+                    <img src="${productsArray.image}" alt="${productsArray.description}" class="img-thumbnail">
+                </div>
+                <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${productsArray.name+"-"+productsArray.cost}</h4>
+                            <small class="text-muted">${productsArray.soldCount} artículos</small>
+                        </div>
+                        <p class="mb-1">${productsArray.description}</p>
+                    </div>
+                </div>
+            </div>
+                `; 
+        }
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+
+
+/**
+ * It sorts the categories array and then shows the sorted categories.
+ * @param sortCriteria - Criterio de ordenamiento
+ * @param categoriesArray - Array of categories to sort.
+ */
+function sortAndShowCategories(sortCriteria, categoriesArray){
+    currentSortCriteria = sortCriteria;
+
+    if(categoriesArray != undefined){
+        currentCategoriesArray = categoriesArray;
+    }
+
+    currentCategoriesArray = sortCategories(currentSortCriteria, currentCategoriesArray);
+
+    //Muestro las categorías ordenadas
+    showCategoriesList();
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            sortAndShowCategories(ORDER_ASC_BY_PRICE, resultObj.data.products);
+        }
+    });
+
+    document.getElementById("sortAsc").addEventListener("click", function(){
+        sortAndShowCategories(ORDER_ASC_BY_PRICE);
+    });
+
+    document.getElementById("sortDesc").addEventListener("click", function(){
+        sortAndShowCategories(ORDER_DESC_BY_PRICE);
+    });
+
+    document.getElementById("sortByCount").addEventListener("click", function(){
+        sortAndShowCategories(ORDER_BY_PROD_REL);
+    });
+
+    document.getElementById("clearRangeFilter").addEventListener("click", function(){
+        document.getElementById("rangeFilterCountMin").value = "";
+        document.getElementById("rangeFilterCountMax").value = "";
+
+        minCount = undefined;
+        maxCount = undefined;
+
+        showCategoriesList();
+    });
+
+    document.getElementById("rangeFilterCount").addEventListener("click", function(){
+        //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
+        //de productos por categoría.
+        minCount = document.getElementById("rangeFilterCountMin").value;
+        maxCount = document.getElementById("rangeFilterCountMax").value;
+
+        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
+            minCount = parseInt(minCount);
+        }
+        else{
+            minCount = undefined;
+        }
+
+        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
+            maxCount = parseInt(maxCount);
+        }
+        else{
+            maxCount = undefined;
+        }
+
+        showCategoriesList();
+    });
+});
+console.log(currentCategoriesArray)
